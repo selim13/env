ec2a93ce8146c04e7e2122377d8a4c44a2926e63
selim13 <selim013@gmail.com>
2019-05-12 10:55:39 +0300

    add firefox user config

---
+++ firefox/user.js
@@ -0,0 +1,5 @@
+// disable DRM stuff
+user_pref("browser.eme.ui.enabled", false);
+user_pref("media.gmp-eme-adobe.enabled", false);
+user_pref("media.eme.enabled", false);
+user_pref("media.eme.apiVisible", false);

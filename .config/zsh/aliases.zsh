7ed238868ca8413769395abfe4758f2cfafcd3d7
selim13 <selim013@gmail.com>
2021-05-31 23:26:42 +0300

    add nvim zsh alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
-# Colorize commands when possible
+# use nvim in favour of vim if present
+[ -x "$(command -v nvim)" ] && alias vim="nvim" vimdiff="nvim -d"
+
+# colorize commands when possible
 alias ls="ls --color=auto" 
 alias grep="grep --color=auto" 
 alias diff="diff --color=auto"
@@ -16,7 +19,7 @@ alias gs="git status"
 alias gd="git diff"
 alias ga="git add"
 
-# Other
+# other
 alias tree="tree -I '.git|node_modules|.sass-cache' --dirsfirst -aC"
 alias halt="halt -p"
 
